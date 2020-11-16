9da7d83590eb44b8e2c22f45a1c5ad5c8d0f73ad
b2smarketing <valente@b2smarketing.com>
2020-11-16 16:38:34 -0300

    16-11-2020_16:38:34

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"Ap,@)vX-y<~1K(]R<kF8jG:!9mi#[WkY/)z<O$pbbS$x}Ie*VNFGDAV#jwJ^Zzmx";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2432;}
